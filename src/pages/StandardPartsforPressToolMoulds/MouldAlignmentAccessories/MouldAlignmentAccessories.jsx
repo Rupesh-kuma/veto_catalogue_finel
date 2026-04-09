880345d05790f45c9ca8633a105d5ae17d600c5d
Rupesh kumar <131702250+Rupesh-kuma@users.noreply.github.com>
2026-04-09 18:16:08 +0000

    update

--- src/pages/StandardPartsforPressToolMoulds/MouldAlignmentAccessories/MouldAlignmentAccessories.jsx
+++ src/pages/StandardPartsforPressToolMoulds/MouldAlignmentAccessories/MouldAlignmentAccessories.jsx
@@ -1,38 +1,38 @@
-import DoubleInterlock from "../ThreePlateMouldAccessories/TaperInterlock/DoubleInterlock";
-import GuideBar from "../ThreePlateMouldAccessories/TaperInterlock/GuideBar";
-import RoundTaperInterlockTaper from "../ThreePlateMouldAccessories/TaperInterlock/RoundTaperInterlockTaper(VB6.1)";
-import StraightBlockSet from "../ThreePlateMouldAccessories/TaperInterlock/StraightBlockSet";
-import TaperBlockSet from "../ThreePlateMouldAccessories/TaperInterlock/TaperBlockSet(VB6.3)";
-import BallCagebush from "./BallCageBush";
-import EjectorGuidePillar1 from "./EjectorGuidePillar/EjectorGuidePillar1";
-import EjectorGuidePillar2 from "./EjectorGuidePillar/EjectorGuidePillar2";
-import EjectorGuidePillar5 from "./EjectorGuidePillar/EjectorGuidePillar5";
-import GuideBushes from "./GuideBushes/GuideBushes";
-import GuideBushes2 from "./GuideBushes/GuideBushes2";
-import GuideBushes3 from "./GuideBushes/GuideBushes3";
-import GuidePillar12 from "./GuidePillar12";
-import GuidePillar12WOG from "./GuidePillar12wog";
-import GuidePillar22 from "./GuidePillar22";
-import GuidePillar22WOG from "./GuidePillar22wog";
-import GuidePillar32 from "./GuidePillar32";
-import GuidePillar32WOG from "./GuidePillar32wog";
-import GuidePillar42 from "./GuidePillar42";
-import GuidePillar42WOG from "./GuidePillar42wog";
-import GuidePillars from "./GuidePillars";
-import GuidePillars2 from "./GuidePillars2";
-import GuidePillars3 from "./GuidePillars3";
-import GuidePillars4 from "./GuidePillars4";
-import GuidePillarsV3 from "./GuidePillarsV3";
-import GuidePillarsV3wog from "./GuidePillarsV3wog";
-import GuidePillarv4 from "./GuidePillarv4";
-import GuidePillarv4wog from "./GuidePillarv4wog";
-import GuidePillarwithoutOilGroove from "./GuidePillarwithoutOilGroove";
-import GuidePillarwithoutOilGroove2 from "./GuidePillarwithoutOilGroove2";
-import GuidePillarWithoutOilGroove3 from "./GuidePillarwithoutOilGroove3";
-import GuidePillarWithoutOilGroove4 from "./GuidePillarwithoutOilGroove4";
-import OillessGuideBushVB3_1 from "./OillessGuideBushes/OillessGuideBush(VB3.1)";
-import OillessGuideBushVB3_2 from "./OillessGuideBushes/OillessGuideBushVB3_2";
-import OillessGuideBushVB3_3 from "./OillessGuideBushes/OillessGuideBushVB3_3";
+// import DoubleInterlock from "../ThreePlateMouldAccessories/TaperInterlock/DoubleInterlock";
+// import GuideBar from "../ThreePlateMouldAccessories/TaperInterlock/GuideBar";
+// import RoundTaperInterlockTaper from "../ThreePlateMouldAccessories/TaperInterlock/RoundTaperInterlockTaper(VB6.1)";
+// import StraightBlockSet from "../ThreePlateMouldAccessories/TaperInterlock/StraightBlockSet";
+// import TaperBlockSet from "../ThreePlateMouldAccessories/TaperInterlock/TaperBlockSet(VB6.3)";
+// import BallCagebush from "./BallCageBush";
+// import EjectorGuidePillar1 from "./EjectorGuidePillar/EjectorGuidePillar1";
+// import EjectorGuidePillar2 from "./EjectorGuidePillar/EjectorGuidePillar2";
+// import EjectorGuidePillar5 from "./EjectorGuidePillar/EjectorGuidePillar5";
+// import GuideBushes from "./GuideBushes/GuideBushes";
+// import GuideBushes2 from "./GuideBushes/GuideBushes2";
+// import GuideBushes3 from "./GuideBushes/GuideBushes3";
+// import GuidePillar12 from "./GuidePillar12";
+// import GuidePillar12WOG from "./GuidePillar12wog";
+// import GuidePillar22 from "./GuidePillar22";
+// import GuidePillar22WOG from "./GuidePillar22wog";
+// import GuidePillar32 from "./GuidePillar32";
+// import GuidePillar32WOG from "./GuidePillar32wog";
+// import GuidePillar42 from "./GuidePillar42";
+// import GuidePillar42WOG from "./GuidePillar42wog";
+// import GuidePillars from "./GuidePillars";
+// import GuidePillars2 from "./GuidePillars2";
+// import GuidePillars3 from "./GuidePillars3";
+// import GuidePillars4 from "./GuidePillars4";
+// import GuidePillarsV3 from "./GuidePillarsV3";
+// import GuidePillarsV3wog from "./GuidePillarsV3wog";
+// import GuidePillarv4 from "./GuidePillarv4";
+// import GuidePillarv4wog from "./GuidePillarv4wog";
+// import GuidePillarwithoutOilGroove from "./GuidePillarwithoutOilGroove";
+// import GuidePillarwithoutOilGroove2 from "./GuidePillarwithoutOilGroove2";
+// import GuidePillarWithoutOilGroove3 from "./GuidePillarwithoutOilGroove3";
+// import GuidePillarWithoutOilGroove4 from "./GuidePillarwithoutOilGroove4";
+// import OillessGuideBushVB3_1 from "./OillessGuideBushes/OillessGuideBush(VB3.1)";
+// import OillessGuideBushVB3_2 from "./OillessGuideBushes/OillessGuideBushVB3_2";
+// import OillessGuideBushVB3_3 from "./OillessGuideBushes/OillessGuideBushVB3_3";
 
 const MouldAlignmentAccessories = () => {
     return(
@@ -49,7 +49,7 @@ const MouldAlignmentAccessories = () => {
                 <div className="product-list my-0 p-3 product_list_main produ_list_hmain">
                     <div className="row row-cols-1 row-cols-3 g-2 mb-4">
                         <div className="col">
-                            <a className="card border-0 bg-transparent pdf-link" href="#catalogue2">
+                            <a className="card border-0 bg-transparent pdf-link" href="https://veto-india.com/wp-content/uploads/2026/04/Guide-Pillar.pdf">
                                 <img src="../images/GuidePillars_main.webp" alt="Product 1" className="card-img-top" />
                                 <div className="card-body bg-white position-absolute  w-100" style={{ top: "93%" }}>
                                     <p className="card-title text-center text-dark mb-0">Guide Pillars</p>
@@ -57,7 +57,7 @@ const MouldAlignmentAccessories = () => {
                             </a>
                         </div>
                         <div className="col">
-                            <a className="card border-0 bg-transparent pdf-link" href="#catalogue22">
+                            <a className="card border-0 bg-transparent pdf-link" href="https://veto-india.com/wp-content/uploads/2026/04/Guide-Bush.pdf">
                                 <img src="../images/GuideBushes_main.webp" alt="Tapped-End-Clamps" className="card-img-top" />
                                 <div className="card-body bg-white position-absolute  w-100" style={{ top: "93%" }}>
                                     <p className="card-title text-center text-dark mb-0">Guide Bushes</p>
@@ -66,17 +66,17 @@ const MouldAlignmentAccessories = () => {
                             </a>
                         </div>
 
-                        <div className="col">
+                        {/* <div className="col">
                             <a className="card border-0 bg-transparent pdf-link" href="#catalogue25">
                                 <img src="../images/EjectorGuidePillar_main.webp" alt="Product 1" className="card-img-top" />
                                 <div className="card-body bg-white position-absolute  w-100" style={{ top: "93%" }}>
                                     <p className="card-title text-center text-dark mb-0">Ejector Guide Pillar</p>
                                 </div>
                             </a>
-                        </div>
+                        </div> */}
 
                          <div className="col">
-                            <a className="card border-0 bg-transparent pdf-link" href="#catalogue28">
+                            <a className="card border-0 bg-transparent pdf-link" href="https://veto-india.com/wp-content/uploads/2026/04/Oilless-Bushes.pdf">
                                 <img src="../images/OillessGuideBushes_main.webp" alt="Product 1" className="card-img-top" />
                                 <div className="card-body bg-white position-absolute  w-100" style={{ top: "93%" }}>
                                     <p className="card-title text-center text-dark mb-0">Oilless Guide Bushes</p>
@@ -84,7 +84,7 @@ const MouldAlignmentAccessories = () => {
                             </a>
                         </div>
                         <div className="col">
-                            <a className="card border-0 bg-transparent pdf-link" href="#catalogue31">
+                            <a className="card border-0 bg-transparent pdf-link" href="https://veto-india.com/wp-content/uploads/2026/04/Tapper-Inter-Lock.pdf">
                                 <img src="../images/TapperInterLock_main.webp" alt="Product 1" className="card-img-top" />
                                 <div className="card-body bg-white position-absolute  w-100" style={{ top: "93%" }}>
                                     <p className="card-title text-center text-dark mb-0">Tapper Inter Lock</p>
@@ -92,7 +92,7 @@ const MouldAlignmentAccessories = () => {
                             </a>
                         </div>
                         <div className="col">
-                            <a className="card border-0 bg-transparent pdf-link" href="#catalogue36">
+                            <a className="card border-0 bg-transparent pdf-link" href="https://veto-india.com/wp-content/uploads/2026/04/Ball-Guide-Bush.pdf">
                                 <img src="../images/ballcase_main.webp" alt="Product 1" className="card-img-top" />
                                 <div className="card-body bg-white position-absolute  w-100" style={{ top: "93%" }}>
                                     <p className="card-title text-center text-dark mb-0">Ball Cage Bush</p>
@@ -109,7 +109,7 @@ const MouldAlignmentAccessories = () => {
             </div>
         </div>
         {/* Guide Pillars */}
-        <GuidePillars/>
+        {/* <GuidePillars/>
         <GuidePillars2/>
         <GuidePillars3/>
         <GuidePillars4/>
@@ -128,33 +128,33 @@ const MouldAlignmentAccessories = () => {
         <GuidePillarsV3/>
         <GuidePillarsV3wog/>
         <GuidePillarv4/>
-        <GuidePillarv4wog/>
+        <GuidePillarv4wog/> */}
 
         {/* Guide Bushes */}
 
-        <GuideBushes/>
+        {/* <GuideBushes/>
         <GuideBushes2/>
-        <GuideBushes3/>
+        <GuideBushes3/> */}
 
         {/* Ejector Guide Pillar */}
 
-        <EjectorGuidePillar1/>
+        {/* <EjectorGuidePillar1/>
         <EjectorGuidePillar2/>
-        <EjectorGuidePillar5/>
+        <EjectorGuidePillar5/> */}
 
         {/* Oilless Guide Bushes */}
 
-        <OillessGuideBushVB3_1/>
+        {/* <OillessGuideBushVB3_1/>
         <OillessGuideBushVB3_2/>
-        <OillessGuideBushVB3_3/>
+        <OillessGuideBushVB3_3/> */}
 
         {/* Tapper Inter Lock */}
-        <RoundTaperInterlockTaper/>
+        {/* <RoundTaperInterlockTaper/>
         <TaperBlockSet/>
         <StraightBlockSet/>
         <DoubleInterlock/>
         <GuideBar/>
-        <BallCagebush/>
+        <BallCagebush/> */}
         </>
     )
 }
